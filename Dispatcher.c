f4bd305dc9fa785e995beb79bc96066742ac5aad
T-Ginger-J <trent.jordan@ontariotechu.net>
2024-03-10 14:49:44 -0400

    header file + structs

---
+++ Dispatcher.c
@@ -0,0 +1,14 @@
+#include <Dispatcher.h>
+
+Process* readDispatchList (char* fileName, int count){
+
+}
+
+// Queue logic here : add, remove, print and sort
+
+// resource logic here : block and free resources
+
+void runDispatcher (Process* jobList, int count){
+
+}
+
---
+++ Dispatcher.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <stdarg.h>
+
+typdef struct {
+	int print;
+	int scan;
+	int modem;
+	int cd;
+} IOdev;
+
+typedef struct {
+	int arrivalT;
+	int priority; //0 RT, 1-3 User
+	int execT; 
+	int memMB;
+	IOdev io; 
+} Process;
+
+Process* readDispatchList(char* fileName, int count);
+
+void runDispatcher(Process* jobList, int count); 
